9ddf4bb9ab5d71453e77f5355b40936463a33ee0
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-11-09 12:13:21 +0530

    Create mv.js

---
+++ plugins/mv.js
@@ -0,0 +1,41 @@
+const config = require('../config')
+
+const {cmd , commands} = require('../command')
+
+
+
+cmd({
+
+    pattern: "movie",
+    alias: ["sub","mv","mv2"],
+    desc: "Movie download.",
+    react: "❗",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+
+try{
+
+
+
+let about = `
+❗If you are not a premierum user 🚫						
+
+🚫ඔබ Premierum User කෙනෙකු නොවේ.
+
+💸 please contact us and purchase the movie download facility                                                                                                              
+
+🪀Contact Owner : https://wa.me/94711451319`
+
+return await conn.sendMessage(from,{image: {url:`https://files.catbox.moe/2elpxs.png`},caption:about},{quoted: mek})
+
+}catch(e){
+
+console.log(e)
+
+reply(`${e}`)
+
+}
+
+})
