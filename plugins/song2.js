cee136e2d2280cf20e6a54830b805d8f568ea77c
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-12-07 21:08:25 +0530

    Create song2.js

---
+++ plugins/song2.js
@@ -0,0 +1,51 @@
+const config = require('../config');
+const { cmd } = require('../command');
+const { ytsearch, ytmp3, ytmp4 } = require('@dark-yasiya/yt-dl.js'); 
+
+
+cmd({
+    pattern: "song2",
+    alias: ["ytmp3","ytsong"],
+    react: "🎶",
+    desc: "Download Youtube song",
+    category: "download",
+    use: '.song < Yt url or Name >',
+    filename: __filename
+},
+async(conn, mek, m,{ from, prefix, quoted, q, reply }) => {
+try{
+
+if(!q) return await reply("Please give me Yt url or Name")
+	
+const yt = await ytsearch(q);
+if(yt.results.length < 1) return reply("Results is not found !")
+
+let yts = yt.results[0]  
+const ytdl = await ytmp3(yts.url)
+		
+let ytmsg = `🎶 NADEEN-MD SONG DOWNLOADER 🎶
+
+
+🎵 *TITLE :* ${yts.title}
+🤵 *AUTHOR :* ${yts.author.name}
+⏱ *RUNTIME :* ${yts.timestamp}
+👀 *VIEWS :* ${yts.views}
+🖇️ *URL :* ${yts.url}
+
+> *▫ NADEEN-MD*
+`
+// SEND DETAILS
+await conn.sendMessage(from, { image: { url: yts.thumbnail || yts.image || '' }, caption: `${ytmsg}`}, { quoted: mek });
+
+// SEND AUDIO TYPE
+await conn.sendMessage(from, { audio: { url: ytdl.download.url }, mimetype: "audio/mpeg" }, { quoted: mek })
+
+// SEND DOC TYPE
+await conn.sendMessage(from, { document: { url: ytdl.download.url }, mimetype: "audio/mpeg", fileName: ytdl.result.title + '.mp3', caption: `${ytdl.result.title}` }, { quoted: mek })
+
+
+} catch (e) {
+console.log(e)
+reply(e)
+}}
+)
