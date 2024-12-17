d0ea0bbcebcb12a1a1fd05a9153454baf2dea03c
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-12-17 15:15:21 +0530

    Create auto.js

---
+++ plugins/auto.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const {readEnv} = require('../lib/database')
+const {cmd , commands} = require('../command')
+
+
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../media/autosticker.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            const config = await readEnv();
+            if (config.AUTO_STICKER === 'true') {
+                //if (isOwner) return;        
+                await conn.sendMessage(from,{sticker: { url : data[text]},package: 'yourName'},{ quoted: mek })   
+            
+            }
+        }
+    }                
+});
+
+//auto reply 
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../media/autoreply.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            const config = await readEnv();
+            if (config.AUTO_REPLY === 'true') {
+                if (isOwner) return;        
+                await m.reply(data[text])
+            
+            }
+        }
+    }                
+}); 
