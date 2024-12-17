10dbd0ab36504a4f12757993a6cf087598104ec8
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-12-17 15:57:40 +0530

    Update and rename auto.js to reply.js

--- plugins/reply.js
+++ plugins/reply.js
@@ -3,26 +3,6 @@ const path = require('path');
 const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
 
-
-cmd({
-  on: "body"
-},    
-async (conn, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../media/autosticker.json');
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    for (const text in data) {
-        if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
-            if (config.AUTO_STICKER === 'true') {
-                //if (isOwner) return;        
-                await conn.sendMessage(from,{sticker: { url : data[text]},package: 'yourName'},{ quoted: mek })   
-            
-            }
-        }
-    }                
-});
-
-//auto reply 
 cmd({
   on: "body"
 },    
