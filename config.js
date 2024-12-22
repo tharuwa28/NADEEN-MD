c8486ad19a548453405e6cce9dcd35c8ad91d26e
tharuwa28 <120113674+tharuwa28@users.noreply.github.com>
2024-12-22 10:52:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
+SESSION_ID: process.env.SESSION_ID || "ZgJGjITY#y9qc8wCViX9vsJxWIAIfdS46l7Eakl3F6M4oP9bzN0k", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 AUTO_VOICE:"true" //true or false
